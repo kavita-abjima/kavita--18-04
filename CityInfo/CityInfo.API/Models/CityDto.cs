13876812e834d27126eb290adb62742507f80935
Bhawani Singh <bhawani.singh@abjima.com>
2023-04-18 18:25:15 +0530

    Created returning resources

---
+++ CityInfo/CityInfo.API/Models/CityDto.cs
@@ -0,0 +1,22 @@
+﻿ namespace CityInfo.API.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointOfInterests.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointOfInterests  { get; set; }
+               = new List<PointOfInterestDto>();
+        public List<PointOfInterestDto>? PointsOfInterest { get; internal set; }
+    }
+}
+
